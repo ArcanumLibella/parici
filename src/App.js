ae6c1db4a19ae78630ab24e4104631046530bb93
Fiona <fiona@MacBook-Pro-de-Fiona-2.local>
2020-01-31 18:16:16 +0100

    WIP: Connection to API Green Spaces

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import axios from "axios";
+import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './assets/css/app.scss';
 
@@ -7,6 +7,25 @@ import './assets/css/app.scss';
 import { Landing, WebApp } from "./routes";
 
 function App() {
+  // const [currentPath, setCurrentPath] = useState("/popular");
+  // const [greenSpaces, setGreenSpaces] = useState();
+
+  // const fetchGreenSpaces = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       "https://opendata.paris.fr/api/records/1.0/search/?dataset=espaces_verts&facet=type_ev&facet=categorie&facet=adresse_codepostal&facet=presence_cloture&facet=ouvert_ferme&refine.type_ev=Promenades+ouvertes" + currentPath
+  //     );
+  //     setMovies(response.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchMovies();
+  // }, [currentPath]);
+
+
   return (
     <Router>
       <div className="App">
