86283c281a000f35f62b1685f12978af000616ae
InSecker <pierresecher98@gmail.com>
2020-02-11 10:59:37 +0100

    api call for sports list

--- src/App.js
+++ src/App.js
@@ -10,77 +10,102 @@ import { Landing, Home } from "./routes";
 // DATA
 let sportsList2 = require('./data/sportsList2');
 
-
-
 function App() {
   // STATE
   const [sportsList, setSportsList] = useState()
 
   // API
-  let host = 'http://localhost:8000'
+  const host = 'http://localhost:8000'
   const apiHeader = { headers: { accept: '*/json' } }
   const apiFamilies = '/api/families'
   const apiCompetitions = '/api/competitions'
-
   const requestFamilies = axios.get(host + apiFamilies, apiHeader)
   const requestCompetitions = axios.get(host + apiCompetitions, apiHeader)
 
-  const fetchSportsList = () => {
-    axios
-      .all([requestFamilies, requestCompetitions])
-      .then(
-        axios.spread((...responses) => {
-          const responseFamilies = responses[0];
-          const responseCompetitions = responses[1];
+  axios
+    .all([requestFamilies, requestCompetitions])
+    .then(axios.spread((...res) => {
+      // Responses arrays
+      const families = res[0].data
+      const sports = res[1].data
 
-          const dataFamilies = responseFamilies.data
-          const dataCompetitions = responseCompetitions.data
-          console.log(dataFamilies)
-          
-          sportsList2.sportsCategory.map(
-            (sportCategory, indexCategory) => {
-              dataFamilies.map(
-                (family, indexFamily) => {
-                  if (indexCategory === indexFamily) {
-                    sportCategory.CategoryName = family.name
-                  }
-                  dataCompetitions.map(
-                    (competition, indexCompetition) => {
-                      // console.log('family', family.idFamily)
-                      // console.log('competition',  parseInt(competition.idFamily.split('/')[3]))
+      families.forEach((family, i) => {
+        // Set category name key/value
+        sportsList2.sportsCategory[i].name = family.name
 
-                      if (family.idFamily === parseInt(competition.idFamily.split('/')[3])) {
-                        // FIXME: pourvoir ajouter chaque sport au lieu de les écraser
-                        sportCategory.sports = Object.assign(
-                          {}, 
-                          {
-                            sportId: competition.idCompetition,
-                            sportName: competition.name
-                          }
-                        )
-                      }
-                    }
-                  )
-                }
-              )
-              // console.log(sportsList2.sportsCategory)
-            }
-          )
-
-          console.log(dataCompetitions)
-          console.log(sportsList2.sportsCategory)
-
-          setSportsList(sportsList2.sportsCategory)
+        let sportsInFamily = []
+        sports.forEach((sport) => {
+          // The api response with route instead of id
+          // Fix later
+          const familyId = sport.idFamily.replace("/api/families/", "")
+          if (familyId == sportsList2.sportsCategory[i].categoryId) {
+            sportsInFamily.push(sport.name)
+            // We attribute that sport to a family so no need to keep it in sports array
+            sports.slice(sport, 1)
+          }
         })
-      )
-      .catch(errors => {
-        console.error(errors);
+        // Set sports for a family
+        sportsList2.sportsCategory[i].sports = sportsInFamily
       });
-  }
+    }))
 
-  useEffect(() => {
-    fetchSportsList()
-  }, [])
+
+  // const fetchSportsList = () => {
+  //   axios
+  //     .all([requestFamilies, requestCompetitions])
+  //     .then(
+  //       axios.spread((...responses) => {
+  //         const responseFamilies = responses[0];
+  //         const responseCompetitions = responses[1];
+
+  //         const dataFamilies = responseFamilies.data
+  //         const dataCompetitions = responseCompetitions.data
+  //         //console.log(dataFamilies)
+          
+  //         sportsList2.sportsCategory.map(
+  //           (sportCategory, indexCategory) => {
+  //             dataFamilies.map(
+  //               (family, indexFamily) => {
+  //                 if (indexCategory === indexFamily) {
+  //                   sportCategory.CategoryName = family.name
+  //                 }
+  //                 dataCompetitions.map(
+  //                   (competition, indexCompetition) => {
+  //                     // //console.log('family', family.idFamily)
+  //                     // //console.log('competition',  parseInt(competition.idFamily.split('/')[3]))
+
+  //                     if (family.idFamily === parseInt(competition.idFamily.split('/')[3])) {
+  //                       // FIXME: pourvoir ajouter chaque sport au lieu de les écraser
+  //                       sportCategory.sports = Object.assign(
+  //                         {}, 
+  //                         {
+  //                           sportId: competition.idCompetition,
+  //                           sportName: competition.name
+  //                         }
+  //                       )
+  //                     }
+  //                   }
+  //                 )
+  //               }
+  //             )
+  //             // //console.log(sportsList2.sportsCategory)
+  //           }
+  //         )
+
+  //         //console.log(dataCompetitions)
+  //         //console.log(sportsList2.sportsCategory)
+
+  //         setSportsList(sportsList2.sportsCategory)
+  //       })
+  //     )
+  //     .catch(errors => {
+  //       console.error(errors);
+  //     });
+  // }
+
+  // useEffect(() => {
+  //   fetchSportsList()
+  // }, [])
 
   return (
     <Router>
