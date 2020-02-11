d0eeb97894740768c046a886486abb56f210af97
ArcanumLibella <fiona.roux@hetic.net>
2020-02-11 11:41:14 +0100

    FEATURE: Add axios sportList Pierre

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
-import React from 'react';
-// import axios from "axios";
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 import './assets/css/app.scss';
 
 // ROUTES
 import { Landing, Home } from "./routes";
 
+// DATA
+let sportsList2 = require('./data/sportList2');
+
 function App() {
+  // STATE
+  const [sportsList, setSportsList] = useState()
+
+  // API
+  const host = 'http://localhost:8000'
+  const apiHeader = { headers: { accept: '*/json' } }
+  const apiFamilies = '/api/families'
+  const apiCompetitions = '/api/competitions'
+  const requestFamilies = axios.get(host + apiFamilies, apiHeader)
+  const requestCompetitions = axios.get(host + apiCompetitions, apiHeader)
+
+  axios
+    .all([requestFamilies, requestCompetitions])
+    .then(axios.spread((...res) => {
+      // Responses arrays
+      const families = res[0].data
+      const sports = res[1].data
+
+      families.forEach((family, i) => {
+        // Set category name key/value
+        sportsList2.sportsCategory[i].name = family.name
+
+        let sportsInFamily = []
+        sports.forEach((sport) => {
+          // The api response with route instead of id
+          // FIXME: later
+          const familyId = sport.idFamily.replace("/api/families/", "")
+          if (familyId == sportsList2.sportsCategory[i].categoryId) {
+            sportsInFamily.push(sport.name)
+            // We attribute that sport to a family so no need to keep it in sports array
+            sports.slice(sport, 1)
+          }
+        })
+        // Set sports for a family
+        sportsList2.sportsCategory[i].sports = sportsInFamily
+        // setSportsList(sportsInFamily)
+      });
+    }))
+
+  // useEffect(() => {
+  //   fetchSportsList()
+  // }, [])
+
   return (
     <Router>
       <div className="App">
         <Switch>
           <Route path="/home">
+            {/* {sportsList
+              ? <Home sportsList={sportsList} />
+              : <div className='loading'>LOADING...</div>
+            } */}
             <Home />
           </Route>
           <Route path="/">
