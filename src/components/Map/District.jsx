8b5512947adacab497ccbc7a616d0811f4a6493a
InSecker <pierresecher98@gmail.com>
2020-02-27 21:20:15 +0100

    cleane up + scale on map

--- src/components/Map/District.jsx
+++ src/components/Map/District.jsx
@@ -8,7 +8,7 @@ const District = ({ number, points, color, onDistrictClick }) => {
   }
 
   return (
-    <path onClick={() => { handleClick() }} id={number} data-name="20" className="cls-2" d={points} style={{ fill: color }} />
+    <path onClick={() => { handleClick() }} id={number} data-name="20" className="cls-2" d={points} style={{ fill: color }} fill="rgb(68,82,197)" />
   )
 }
 
